ce5ca048751b294c9bb3c4d90a8a86ce0153207f
Pankaj Adhyapak <pankaj@Pankajs-MacBook-Air.local>
2016-10-28 01:12:35 +0530

    Avatars Path FIx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class MyApp {
       this.rootPage = HomePage;
       this.loggedIn = true;
       this.loggedUser = JSON.parse(window.localStorage['loggenUser']);
-      
+
     } else {
       this.rootPage = LoginPage;
       this.loggedIn = false;
@@ -103,6 +103,8 @@ export class MyApp {
   initializeApp() {
     this.platform.ready().then(() => {
       StatusBar.styleDefault();
+      StatusBar.styleLightContent();
+      Splashscreen.hide();
       if(this.loggedIn){
         //this.generateToken();
       }
